0e03ebff9df74bd4ae7af9a3b4aa8ec834a05b64
Gabriel Torres <127896538+Gabe-Torres@users.noreply.github.com>
2024-09-07 16:34:48 -0500

    seed data for 10 tables
    adds a sample set for 10 tables with seating capacity ranging from 2-8 people

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,15 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Table.create([
+  { number: "T1", capacity: 2 },
+  { number: "T2", capacity: 2 },
+  { number: "T3", capacity: 4 },
+  { number: "T4", capacity: 4 },
+  { number: "T5", capacity: 6 },
+  { number: "T6", capacity: 6 },
+  { number: "T7", capacity: 8 },
+  { number: "T8", capacity: 8 },
+  { number: "T9", capacity: 4 },
+  { number: "T10", capacity: 2 }
+])
