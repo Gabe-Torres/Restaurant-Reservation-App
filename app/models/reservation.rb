8123659c5e3765803c8a61000185cf7c1e8f1757
Gabriel Torres <127896538+Gabe-Torres@users.noreply.github.com>
2024-09-07 16:15:35 -0500

    add - model associations and attributes
    adds associations and attributes for: Reservation, Table, and ReservationTable

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,2 +1,19 @@
+# == Schema Information
+#
+# Table name: reservations
+#
+#  id               :bigint           not null, primary key
+#  name             :string
+#  party_size       :integer
+#  reservation_time :datetime
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#
 class Reservation < ApplicationRecord
+  has_many :reservation_tables
+  has_many :tables, through: :reservation_tables
+
+  validates :name, presence: true
+  validates :party_size, presence: true, numericality: { only_integer: true, greater_than: 0 }
+  validates :reservation_time, presence: true
 end
