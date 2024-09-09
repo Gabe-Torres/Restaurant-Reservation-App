199823e8c6cadbbb7b8244860b18d6cefa8a3e11
Gabriel Torres <127896538+Gabe-Torres@users.noreply.github.com>
2024-09-09 17:50:10 -0500

    add test for model and controller methods
    - adds model method test for the following methods:
    -- reserve_tables
    -- formatted_reservation_time
    -- available_table
    
    - adds controller method test for the following method:
    -- generate_future_times

--- spec/models/reservation_spec.rb
+++ spec/models/reservation_spec.rb
@@ -1,6 +1,27 @@
+# == Schema Information
+#
+# Table name: reservations
+#
+#  id               :bigint           not null, primary key
+#  name             :string
+#  party_size       :integer
+#  reservation_time :datetime
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#
 require 'rails_helper'
 
 RSpec.describe Reservation , type: :model do
+  before(:each) do
+    @reservation_time = Time.current + 1.hour
+    @table = Table.create!(capacity: 4, number: "T1")
+  end
+  
+  after(:each) do
+    Table.delete_all
+    Reservation.delete_all
+  end
+
   context 'Associations' do
     it "has associations" do
       should have_many(:reservation_tables)
@@ -14,6 +35,21 @@ RSpec.describe Reservation , type: :model do
       should validate_presence_of(:party_size)
       should validate_numericality_of(:party_size).only_integer.is_greater_than(0)
       should validate_presence_of(:reservation_time)
+    end
+  end
+
+  describe '#reserve_tables' do
+    it 'assigns available tables to the reservation' do
+      reservation = Reservation.new(name: "Shane O'Malley", party_size: 2, reservation_time: @reservation_time)
+      reservation.reserve_tables
+      expect(reservation.tables).to include(@table)
+    end
+
+    it 'does not assign tables if none are available' do
+      Table.delete_all
+      reservation = Reservation.new(name: "Shane O'Malley", party_size: 2, reservation_time: @reservation_time)
+      reservation.reserve_tables
+      expect(reservation.tables).to be_empty
     end
   end
 end
--- spec/models/table_spec.rb
+++ spec/models/table_spec.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: tables
+#
+#  id         :bigint           not null, primary key
+#  capacity   :integer
+#  number     :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 require 'rails_helper'
 
 RSpec.describe Table, type: :model do
@@ -13,6 +23,22 @@ RSpec.describe Table, type: :model do
       should validate_presence_of(:capacity)
       should validate_numericality_of(:capacity).only_integer.is_greater_than(0)
       should validate_presence_of(:number)
+    end
+  end
+
+  describe '.available_table' do
+    before(:each) do
+      @reservation_time = Time.current + 1.hour
+    end
+
+    it 'returns the first available table based on party size and reservation time' do
+      table = Table.create!(capacity: 4, number: "T1")
+      expect(Table.available_table(2, @reservation_time)).to eq(table)
+    end
+
+    it 'returns nil if no tables are available' do
+      Table.delete_all
+      expect(Table.available_table(2, @reservation_time)).to be_nil
     end
   end
 end
