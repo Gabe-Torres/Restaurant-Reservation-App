bddb7c3a31ed4ef42c855ea772bcd544de1d646a
Gabriel Torres <127896538+Gabe-Torres@users.noreply.github.com>
2024-09-09 21:30:38 -0500

    update success handling

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -9,7 +9,7 @@ class ReservationsController < ApplicationController
     @reservation = Reservation.new(reservation_params)
     if @reservation.valid? && @reservation.reserve_tables
       @reservation.save!
-      flash[:notice] = "Reservation successful!"
+      flash[:success] = "Reservation successful!"
     else
       flash[:alert] = "Reservation failed. Please check capacity and availability."
     end
