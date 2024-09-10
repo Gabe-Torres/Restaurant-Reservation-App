53f6745e6bfcc85ac5f39dbf5d05516a36401f90
Gabriel Torres <127896538+Gabe-Torres@users.noreply.github.com>
2024-09-09 20:37:44 -0500

    set up bootstrap and improve user experience

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,17 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+import "bootstrap"
+import "../stylesheets/application.scss"
+
+import { Tooltip, Toast, Popover } from 'bootstrap';
+
+// Initialize tooltips
+document.querySelectorAll('[data-bs-toggle="tooltip"]').forEach((element) => {
+  new Tooltip(element);
+});
+
+// Initialize popovers
+document.querySelectorAll('[data-bs-toggle="popover"]').forEach((element) => {
+  new Popover(element);
+});
